812c4e08fe67d74c571847c39b75099481c52b6d
MeghanaDwasari <meghanadwasari@gmail.com>
2024-11-22 14:54:16 +0530

    commited by me

---
+++ prime.java
@@ -0,0 +1,27 @@
+public class Prime
+{
+    public static void main(String args[])
+    {
+      int num=29
+      boolean isprime=true;
+      for(int i=2;i<=n/2;i++
+      {
+
+      
+         if(num%i==0)
+        {
+          isprime=false;
+           break;
+        }
+
+    }
+      if(isprime)
+     {
+        System.out.println(+num"it is prime number");
+     }
+    else
+     {
+        System.out.println(+num"it is not a prime number");
+     }
+   }
+}
